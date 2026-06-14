c753bf0214c22aeaf9a9df0de8d56a640d42b119
Gautam Sharma <gautamab2005@gmail.com>
2026-06-14 17:06:35 +0530

    Organized basic string problems

---
+++ main.py
@@ -0,0 +1,27 @@
+# ============================================================
+#                    PYTHON PRACTICE
+#                    MAIN TEST FILE
+# ============================================================
+
+"""
+This file is used for:
+
+1. Testing different Python solutions
+2. Running practice code
+3. Experimenting with new logic
+
+All final solutions are organized inside topic-wise folders.
+"""
+
+
+# ============================================================
+#                    PRACTICE AREA
+# ============================================================
+
+
+def main():
+    print("Python Practice Started ")
+
+
+if __name__ == "__main__":
+    main()
